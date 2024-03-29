702d20160d68bc16042ee5b2e528cf679f882e84
qkrrjsxor <156152338+qkrrjsxor@users.noreply.github.com>
2024-03-29 17:38:41 +0900

    [Gold IV] Title: 문자열 폭발, Time: 372 ms, Memory: 94452 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		char[] charArr = br.readLine().toCharArray();
+		char[] bomb = br.readLine().toCharArray();
+		List<Character> list = new ArrayList<>();
+
+		int charLen = charArr.length;
+		int bombLen = bomb.length;
+		int idx = 0 ;
+		int k = bombLen;
+		for (int i = 0; i < charLen; i++) {
+			list.add(charArr[i]);
+//			System.out.println(idx);
+			
+			if(idx >= bombLen-1 && list.get(idx) == bomb[bombLen-1]) {
+				k= bombLen-1;
+//				System.out.println("k : " + k);
+				for(int t = idx; t > idx-bombLen; t--) {
+//					System.out.println("te : " + t);
+					if(list.get(t)==bomb[k]) {
+						k--;
+					}
+				}
+//				System.out.println("kk :" + k);
+				if(k==-1) {
+					for(int t = idx; t > idx-bombLen; t--) {
+						list.remove(t);
+					}
+					idx = idx-bombLen;
+				}
+			}
+			
+			idx++;
+		}
+
+
+
+		if (list.size() == 0) {
+			System.out.println("FRULA");
+		} else {
+			int len = list.size();
+			for (int i = 0; i < len; i++) {
+				sb.append(list.get(i));
+			}
+			System.out.println(sb.toString());
+		}
+	}
+
+}
